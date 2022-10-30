cc5788a7427d66c93f2f3ab7e0bf32dca1efaa2f
Oliver Eckle <iee1394@gmx.de>
2022-10-30 11:52:42 +0100

    extend user for additinal contact infos and add some tests

--- user/src/main/kotlin/de/sambalmueslie/openevent/user/api/User.kt
+++ user/src/main/kotlin/de/sambalmueslie/openevent/user/api/User.kt
@@ -5,8 +5,11 @@ import de.sambalmueslie.openevent.common.crud.BusinessObject
 data class User(
     override val id: Long,
     val externalId: String,
+    val type: UserType,
     val userName: String,
-    val email: String,
     val firstName: String,
     val lastName: String,
+    val email: String,
+    val mobile: String,
+    val phone: String,
 ) : BusinessObject<Long>
--- user/src/main/resources/db/migration/V1__create-user-schema.sql
+++ user/src/main/resources/db/migration/V1__create-user-schema.sql
@@ -3,10 +3,13 @@ CREATE TABLE _user
 (
     id          BIGINT                      NOT NULL PRIMARY KEY DEFAULT nextval('_user_seq'::regclass),
     external_id VARCHAR(255)                NOT NULL,
+    type        VARCHAR(255)                NOT NULL,
     user_name   VARCHAR(255)                NOT NULL,
-    email       VARCHAR(255)                NOT NULL,
     first_name  VARCHAR(255)                NOT NULL,
     last_name   VARCHAR(255)                NOT NULL,
+    email       VARCHAR(255)                NOT NULL,
+    mobile      VARCHAR(255)                NOT NULL,
+    phone       VARCHAR(255)                NOT NULL,
     created     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated     TIMESTAMP WITHOUT TIME ZONE
 );

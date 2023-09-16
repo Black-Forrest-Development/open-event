3f60f9cd7a1b8253b0612a9c7caea936a735c1b3
Oliver Eckle <iee1394@gmx.de>
2023-09-16 15:17:00 +0200

    fix test compile error

--- src/test/kotlin/de/sambalmueslie/openevent/core/logic/export/HtmlConverterTest.kt
+++ src/test/kotlin/de/sambalmueslie/openevent/core/logic/export/HtmlConverterTest.kt
@@ -1,5 +1,6 @@
 package de.sambalmueslie.openevent.core.logic.export
 
+import org.apache.velocity.tools.generic.EscapeTool
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -8,9 +9,8 @@ class HtmlConverterTest {
 
     @Test()
     fun testConverter() {
-        val content =
-            "<p><strong>Fetter Text</strong></p><p><br></p><p><em>Kursiver Text</em></p><p><br></p><p><u>Unterstrichener Text</u></p><p><br></p><p><s>Durchgestrichener Text</s></p><p><br></p><p><span class=\"ql-size-huge\">Großer Text</span></p><p><br></p><h1>Überschrift 1</h1><p>Linksbündig</p><p class=\"ql-align-right\">Rechtsbündig</p><p class=\"ql-align-center\">Zentriert</p><p>adsfasdfd</p><p>asdfsadfsadf</p><p>adsf</p><p><br></p>"
-        val converter = HtmlConverter()
+        val escapeTool = EscapeTool()
+        val converter = HtmlConverter(escapeTool)
 
         assertEquals("<fo:block font-size=\"12pt\">Normaler Text</fo:block>\n", converter.convert("<p>Normaler Text</p>"))
         assertEquals("<fo:block font-weight=\"bold\" font-size=\"12pt\">Fetter Text</fo:block>\n", converter.convert("<p><strong>Fetter Text</strong></p>"))

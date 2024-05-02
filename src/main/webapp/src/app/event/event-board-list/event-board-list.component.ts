91d4866cae4b6ad81269959af077ec03525ca8e6
Oliver Eckle <iee1394@gmx.de>
2024-05-02 23:36:07 +0200

    work on the search board

--- src/main/webapp/src/app/event/event-board-list/event-board-list.component.ts
+++ src/main/webapp/src/app/event/event-board-list/event-board-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {EventBoardService} from "../model/event-board.service";
 
 @Component({
@@ -6,7 +6,45 @@ import {EventBoardService} from "../model/event-board.service";
   templateUrl: './event-board-list.component.html',
   styleUrls: ['./event-board-list.component.scss']
 })
-export class EventBoardListComponent {
-  constructor(public service: EventBoardService) {
+export class EventBoardListComponent implements OnInit, OnDestroy {
+
+  @Input() options = {};
+  @ViewChild('anchor', {static: true}) anchor: ElementRef<HTMLElement> | undefined
+  private observer: IntersectionObserver | undefined
+
+  constructor(public service: EventBoardService, private host: ElementRef) {
   }
+
+  get element() {
+    return this.host.nativeElement;
+  }
+
+  ngOnInit() {
+    const options = {
+      root: this.isHostScrollable() ? this.host.nativeElement : null,
+      ...this.options
+    }
+    if (!this.anchor) return
+
+    this.observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        this.service.onScroll()
+      }
+    }, options);
+
+    this.observer.observe(this.anchor.nativeElement)
+  }
+
+  private isHostScrollable() {
+    const style = window.getComputedStyle(this.element);
+
+    return style.getPropertyValue('overflow') === 'auto' ||
+      style.getPropertyValue('overflow-y') === 'scroll';
+  }
+
+  ngOnDestroy() {
+    if (this.observer) this.observer.disconnect()
+  }
+
+
 }

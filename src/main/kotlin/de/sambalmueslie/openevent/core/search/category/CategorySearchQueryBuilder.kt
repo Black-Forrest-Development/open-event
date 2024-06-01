49537942e2f65c639be5a30e3ffe3cbb5dea3bba
Oliver Eckle <iee1394@gmx.de>
2024-06-01 17:15:36 +0200

    rework category management

--- src/main/kotlin/de/sambalmueslie/openevent/core/search/category/CategorySearchQueryBuilder.kt
+++ src/main/kotlin/de/sambalmueslie/openevent/core/search/category/CategorySearchQueryBuilder.kt
@@ -1,7 +1,6 @@
 package de.sambalmueslie.openevent.core.search.category
 
-import com.jillesvangurp.searchdsls.querydsl.SearchDSL
-import com.jillesvangurp.searchdsls.querydsl.match
+import com.jillesvangurp.searchdsls.querydsl.*
 import de.sambalmueslie.openevent.core.account.api.Account
 import de.sambalmueslie.openevent.core.search.api.CategorySearchRequest
 import de.sambalmueslie.openevent.core.search.common.SearchQueryBuilder
@@ -18,10 +17,16 @@ class CategorySearchQueryBuilder : SearchQueryBuilder<CategorySearchRequest> {
         from = pageable.offset.toInt()
         resultSize = pageable.size
         trackTotalHits = "true"
-        query = match(CategorySearchEntryData::name, request.fullTextSearch) {
-            boost = 2.0
-            lenient = true
-            autoGenerateSynonymsPhraseQuery = true
+        query = bool {
+            if (request.fullTextSearch.isNotBlank()) {
+                put("minimum_should_match", 1)
+                should(
+                    match(CategorySearchEntryData::name, request.fullTextSearch) { boost = 2.0 },
+                    matchBoolPrefix(CategorySearchEntryData::name, request.fullTextSearch)
+                )
+            } else {
+                matchAll()
+            }
         }
     }
 
--- src/main/webapp/src/app/category/category-routing.module.ts
+++ src/main/webapp/src/app/category/category-routing.module.ts
@@ -1,12 +1,9 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {CategoryBoardComponent} from "./category-board/category-board.component";
-import {CategoryChangeComponent} from "./category-change/category-change.component";
 
 const routes: Routes = [
-  {path: '', component: CategoryBoardComponent},
-  {path: 'create', component: CategoryChangeComponent},
-  {path: 'edit/:id', component: CategoryChangeComponent},
+  {path: '', component: CategoryBoardComponent}
 ];
 
 @NgModule({

f4b021242d6d808550a6dc490812b493a5f844e0
Oliver Eckle <iee1394@gmx.de>
2024-08-01 20:48:26 +0200

    minor bugfix

--- src/main/webapp/src/app/registration/registration-participate-manual-dialog/registration-participate-manual-dialog.component.ts
+++ src/main/webapp/src/app/registration/registration-participate-manual-dialog/registration-participate-manual-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
 import {ParticipantAddRequest} from "../model/registration-api";
 
@@ -17,13 +17,16 @@ export class RegistrationParticipateManualDialogComponent {
     private fb: FormBuilder
   ) {
     this.fg = fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', Validators.email],
-      phone: [''],
-      mobile: [''],
-      size: [0, Validators.compose([Validators.required, Validators.min(1)])]
-    });
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', Validators.email],
+        phone: [''],
+        mobile: [''],
+        size: [0, Validators.compose([Validators.required, Validators.min(1)])]
+      },
+      {
+        validators: [atLeastOneRequiredValidator(['email', 'phone', 'mobile'])]
+      });
   }
 
   submit() {
@@ -32,3 +35,26 @@ export class RegistrationParticipateManualDialogComponent {
     this.dialogRef.close(new ParticipantAddRequest(value.firstName, value.lastName, value.email, value.phone, value.mobile, value.size))
   }
 }
+
+
+export function atLeastOneRequiredValidator(controlNames: string[]): ValidatorFn {
+  // @ts-ignore
+  return (formGroup: FormGroup): ValidationErrors | null => {
+    const isValid = controlNames.some(controlName => {
+      const control = formGroup.get(controlName);
+      return control && control.value;
+    });
+
+    // Mark all controls as touched and invalid if none are set
+    controlNames.forEach(controlName => {
+      const control = formGroup.get(controlName);
+      if (control) {
+        control.setErrors(isValid ? null : { required: true });
+      }
+    });
+
+    return isValid ? null : {atLeastOneRequired: true};
+  };
+}
+
+
